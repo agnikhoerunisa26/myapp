9424d776cfb89e3e1c9a8078ee501f8b8d80f0cf
agnikhoerunisa26 <agnikhoerunisa2009@gmail.com>
2024-11-13 08:57:12 +0700

    first commit

---
+++ my app/#program untuk menjumlahkan dua angka.py
@@ -0,0 +1,9 @@
+#program untuk menjumlahkan dua angka
+angka1 = float(input("masukkan angka pertama:  "))
+angka2 = float(input("masukkan angka kedua"))
+
+#proses penjumlahan
+hasil = angka1 + angka2
+
+#menampilkan hasil
+print(f"hasil dari {angka1} + {angka2} adalah {hasil}")
---
+++ my app/Program.cs
@@ -0,0 +1,46 @@
+﻿  Console.WriteLine("Hello, Welcome to matematika!");
+  using System;
+  class program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(1);
+      double angka1 = Convert.ToDouble(Console.ReadLine());
+      
+      Console.WriteLine(2);
+      double angka2 = Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine(pilih operasi (+, -, *, /):);
+      char operasi = Console.ReadKey()KeyChar;
+      Console.WriteLine();
+
+      double hasil = 0;
+
+      switch (operasi)
+      {
+        
+        case '+':
+        hasil = angka1 + angka2;
+        break
+        case '-':
+        hasil = angka1 - angka2;
+        break
+        case '*':
+        hasil = angka1 * angka2;
+        break
+        case '/':
+        if (angka2 !=0)
+      }
+      else
+      {
+        Console.WriteLine(Pembagian dengan nol tidak diizinkan.);
+        return;
+      }
+      break;
+default:
+      Console.WriteLine(operasi tidak dikenali.);
+      return;
+    }
+    Console.WriteLine($Hasil: {hasil});
+  }
+}
